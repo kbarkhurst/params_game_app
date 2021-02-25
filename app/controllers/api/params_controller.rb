84e3d90989bc6843c7d2bad59f244210e1130936
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-02-25 15:30:21 -0500

    Writes A if your name starts with A otherwise writes your name in Caps

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,6 +1,7 @@
 class Api::ParamsController < ApplicationController
   def share_name
     @message = "Share your name."
+    @first_name = params[:fn].upcase
     render "params.json.jb"
   end
 end
